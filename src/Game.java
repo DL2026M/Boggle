793a1ddbd56578181bc3bc452062109bafeb9842
DL2026M <david.lutch@menloschool.org>
2025-04-15 17:46:03 -0700

    Added instance variables, headers, and constructors

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,26 @@
+// Author, David Lutch
+
 public class Game {
+    private boolean gameOver;
+    private int pointValue;
+    private ArrayList<Words> Words;
+
+    public boolean isGameOver() {
+
+    }
+    public int pointValueWord() {
+
+    }
+    public boolean isValidWord() {
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    }
+
+    public Game(boolean gameOver, int pointValue, ArrayList<Words> words) {
+        this.gameOver = gameOver;
+        this.pointValue = pointValue;
+        Words = words;
     }
 }
